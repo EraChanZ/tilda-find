21b9e6dea64bb1b0ff85416c290d9c7a06af9204
EraChanZ <39560922+EraChanZ@users.noreply.github.com>
2018-08-29 13:46:17 +0300

    Update settings_prod.py

--- ProgTinder/settings_prod.py
+++ ProgTinder/settings_prod.py
@@ -5,5 +5,7 @@ DATABASES = {
         'ENGINE':'django.db.backends.postgresql_psycopg2',
         'NAME': 'hello',
         'USER': 'hello_django',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
